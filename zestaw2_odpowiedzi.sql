95e7bf2b9e7e08c0c09a69777352a5f045ac456a
Sylwek <you@example.com>
2025-07-14 07:45:43 +0200

    Update zestaw2_odpowiedzi.sql

--- zestaw2_odpowiedzi.sql
+++ zestaw2_odpowiedzi.sql
@@ -1,2 +1,18 @@
+-- 26
+SELECT * FROM jobs
+WHERE title LIKE 'HR%';
 
+-- 27
+SELECT * FROM jobs
+WHERE title LIKE '%Software%';
+
+-- 28
+SELECT AVG(min_salary)
+AS average_min_salary_sales
+FROM jobs 
+WHERE title LIKE "Sales%";
+
+-- 29
+SELECT * FROM jobs
+WHERE title LIKE '_u%';
 
